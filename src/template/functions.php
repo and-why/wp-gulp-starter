0642854002b8b5632f52e730a53103e6801d7355
Andy Smith <work@andysmith.is>
2018-11-17 12:27:58 +1100

    mobile menu imrpovements

--- src/template/functions.php
+++ src/template/functions.php
@@ -122,8 +122,6 @@ add_action( 'widgets_init', 'wpgulpstarter_widgets_init' );
 function wpgulpstarter_scripts() {
 	wp_enqueue_style( 'wpgulpstarter-style', get_stylesheet_uri() );
 
-
-	
 	wp_enqueue_script( 'wpgulpstarter-scripts', get_template_directory_uri() . '/js/scripts.js', array(), '20151215', true );
 
 
